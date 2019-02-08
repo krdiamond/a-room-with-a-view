f594355ceb33cffc06f777dc0f5ca6d52c819a68
Kristina <krdiamond@gmail.com>
2019-02-08 17:33:54 -0500

    implemented drag and drop functionality

--- src/components/shelf/shelf.vue
+++ src/components/shelf/shelf.vue
@@ -3,15 +3,18 @@
   <div id="shelf-area">
 
 
-    <img id="shelf" :src="shelfImage"/>
+    <img id="shelf" :src="shelfImage" class="draggable"/>
 
     <!-- <img id="watering-can" :src="wateringCanImage"/> -->
 
   </div>
 </template>
 
+
 <script>
 import shelfFile from "./images/shelf.png"
+// import VueInteract from '../../drag-n-drop.js';
+import interact from 'interact.js';
 // import wateringCanFile from "./images/watering-can.png"
 
 export default {
@@ -21,6 +24,30 @@ export default {
       shelfImage: shelfFile,
       // wateringCanImage: wateringCanFile,
     }
+  },
+  mounted() {
+    interact('.draggable')
+    .draggable({
+      onmove: this.dragMoveListener
+    });
+  },
+  methods: {
+    dragMoveListener (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
   }
+
+
+}
 }
 </script>
